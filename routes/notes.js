e670241ac6d3508b25be5d6c475d805ef7ecc27c
Miguel Escobar <mae2136@gmail.com>
2022-04-15 22:41:12 -0500

    V 0.3 - Create note routes.

--- routes/notes.js
+++ routes/notes.js
@@ -3,8 +3,28 @@ const notes = require('express').Router();
 const uniqid = require(`uniqid`);
 
 // GET route to read db.json and return all saved notes as JSON
+notes.get(`/`, (req, res) =>
+
+);
 
 // POST route to receive new note to save on the request body, add to db.json file, and retun the new note to the client. Give each note a unique id when saved.
+notes.post(`/`, (req, res) => {
+    console.log(req.body);
 
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title: title,
+            text: text,
+            id: uniqid(),
+        }
+        // Append newNote to db.json
+        
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error(`Error in adding note`);
+    }
+})
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -12,21 +12,22 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // API routes go here
 app.use(`/api`, api)
 
+// Serves static files (CSS, JS)
 app.use(express.static(`public`));
 
 // Get * route for index.html
-app.get(`*`, (req, res) =>
+app.get(`/*`, (req, res) =>
     res.sendFile(path.join(__dirname, `/public/index.html`))
 );
 
 // Get /notes returns notes.html
 app.get(`/notes`, (req, res) =>
-res.sendFile(path.join(__dirname, `/public/notes.html`))
+    res.sendFile(path.join(__dirname, `/public/notes.html`))
 );
 
 // Server Listen
