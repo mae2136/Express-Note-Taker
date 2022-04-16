2b7e31b1ad92ad520cc2d82312e2dcc1f0bff18b
Miguel Escobar <mae2136@gmail.com>
2022-04-15 19:33:01 -0500

    V 0.2 - Add basic GET routes to server.js. - Add server start/listen to server.js.

--- server.js
+++ server.js
@@ -1,3 +1,31 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
 const fs = require(`fs`);
+
+// Port information.
+const PORT = process.env.PORT || 3001;
+
+// Express
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static(`public`));
+
+// Get * route for index.html
+app.get(`*`, (req, res) =>
+    res.sendFile(path.join(__dirname, `/public/index.html`))
+);
+
+// Get /notes returns notes.html
+app.get(`/notes`, (req, res) =>
+res.sendFile(path.join(__dirname, `/public/notes.html`))
+);
+
+// Server Listen
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
